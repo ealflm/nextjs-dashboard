9a249efb26e72b053188f8e042304d32541e4261
NamDP <ealflm@gmail.com>
2024-04-02 15:20:06 +0700

    WIP

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,6 +36,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+  console.log('Fetching revenue data...');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -43,6 +45,7 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+    console.log('Data fetch completed after 3 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
